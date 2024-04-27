f5c4671d456920cf560ea63c09d4e29d4cbd8690
Luke <22577678+Iuke121@users.noreply.github.com>
2024-04-26 18:54:38 -0700

    Hello world message #3

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,12 +1,11 @@
-import {
-    Fragment,
-} from 'react';
+import { Fragment } from 'react';
 
 export const App = () => {
-    return (
-        <Fragment>
-            <h1>Mockazon</h1>
-            <p>Hello World this is Evan Metcalf</p>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <h1>Mockazon</h1>
+      <p>Hello World this is Evan Metcalf</p>
+      <p>Hello World this is Lukas Teixeira Döpcke</p>
+    </Fragment>
+  );
+};
