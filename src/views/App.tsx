2049353300e69442de37bf7bdf7339e0291a3dff
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-04-26 17:40:55 -0700

    Setup UI Layout

---
+++ src/views/App.tsx
@@ -0,0 +1,12 @@
+import {
+    Fragment,
+} from 'react';
+
+export const App = () => {
+    return (
+        <Fragment>
+            <h1>Mockazon</h1>
+            <p>Hello World this is Evan Metcalf</p>
+        </Fragment>
+    )
+}
