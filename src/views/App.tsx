f998677a61561ecbd58715909c8ff1561cd3b6b1
tryles <trevorryles02@gmail.com>
2024-04-26 19:50:01 -0700

    Add hello world for Trevor Ryles

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -6,6 +6,7 @@ export const App = () => {
       <h1>Mockazon</h1>
       <p>Hello World this is Evan Metcalf</p>
       <p>Hello World this is Lukas Teixeira Döpcke</p>
+      <p>Hello, salute. It&apos;s me... your duke - Trevor Ryles</p>
     </Fragment>
   );
 };
