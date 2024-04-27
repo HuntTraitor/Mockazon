ec4e42ef6b7255f002914fe9304343d6fbe55140
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-04-26 17:41:08 -0700

    Setup Placeholder Testing

---
+++ test/pages/index.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+
+import Index from '../../src/pages/index';
+
+it('Renders', async () => {
+  render(<Index />)
+  await screen.findByText('CSE187 Assignment 3')
+});
---
+++ test/views/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+
+import { App } from '../../src/views/App';
+
+
+it('Renders', async () => {
+  render(<App />)
+});
