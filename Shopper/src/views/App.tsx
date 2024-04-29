064b62df0f76bee2d03b0d32e299442da32e6ba8
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-04-29 15:59:06 -0700

    Implement Icons and Logos and Update .gitignore #35

--- Shopper/src/views/App.tsx
+++ Shopper/src/views/App.tsx
@@ -4,6 +4,16 @@ export const App = () => {
   return (
     <Fragment>
       <h1>Mockazon Shopper App</h1>
+      <img
+        src="/mockazon_logo_white.png"
+        alt="Mockazon Logo"
+        style={{
+          width: '400px',
+          height: 'auto',
+          boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.1)',
+          margin: '20px',
+        }}
+      />
       <p>Hello World this is Evan Metcalf</p>
       <p>Hello World this is Lukas Teixeira Döpcke</p>
       <p>Hello, salute. It&apos;s me... your duke - Trevor Ryles</p>
