2a01af303fa568a1a14c022d454cbab47bf11f60
Luke <22577678+Iuke121@users.noreply.github.com>
2024-04-29 15:34:45 -0700

    Refactor the hello world app #3

---
+++ HelloWorld/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { Fragment } from 'react';
+import { App } from '../views/App';
+
+export default function Index() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Mockazon</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" /> {/*replace with mockazon logo */}
+      </Head>
+      <App/>
+    </Fragment>
+  )
+}
