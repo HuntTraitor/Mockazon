0da0fff850f82067b057fc9c44389f34d965db2d
Luke <22577678+Iuke121@users.noreply.github.com>
2024-05-01 21:22:22 -0700

    Set up post creation endpoint on microservice #20

--- ProductService/src/product/productController.ts
+++ ProductService/src/product/productController.ts
@@ -2,30 +2,84 @@ import {
   Body,
   Controller,
   Post,
-  Response,
-  Request,
   Route,
   SuccessResponse,
-  Path,
+  Query,
   Put,
-} from 'tsoa'
-import { NewProduct, Product } from '.';
-import { ProductService } from './productService';
+  Response,
+  Get,
+  Path
+} from 'tsoa';
+import {NewProduct, Product} from '.';
+import {UUID} from '../types';
+import {ProductService} from './productService';
+
 @Route('product')
 export class ProductController extends Controller {
+  @Get()
+  @SuccessResponse('200', 'Products Retrieved')
+  public async getProducts(
+    @Query() vendorId?: UUID,
+    @Query() active?: boolean,
+    @Query() page?: number,
+    @Query() pageSize?: number,
+    @Query() search?: string,
+    @Query() orderBy?: string,
+    @Query() descending?: boolean,
+  ): Promise<Product[] | undefined> {
+    return await new ProductService().getAll(vendorId, active, page, pageSize, search, orderBy, descending);
+  }
+
+  @Get('{productId}')
+  @SuccessResponse('200', 'Product Retrieved')
+  @Response('404', 'Product Not Found')
+  public async getProduct(
+    @Path() productId: UUID
+  ): Promise<Product | undefined> {
+    const product = await new ProductService().getOne(productId);
+    if (!product) {
+      this.setStatus(404);
+    }
+    return product;
+  }
+
   @Post()
   @SuccessResponse('201', 'Product Created')
   public async createProduct(
     @Body() product: NewProduct,
-  ): Promise<Product|undefined> {
-    return await new ProductService().create(product, request.user?.id);
+    @Query() vendorId: UUID
+  ): Promise<Product | undefined> {
+    // FIXME: Do we want to stop duplicate products from being created? As in same vendor ID same name?
+    // Could address this in the Vendor API.
+    return await new ProductService().create(product, vendorId);
   }
 
-  @Put('{id}')
-  @SuccessResponse('200', 'Product Disabled')
-  public async disableProduct(
-    @Path() id: string,
-  ): Promise<Product|undefined> {
-    return await new ProductService().disable(id, request.user?.id);
+  @Put('{productId}')
+  @SuccessResponse('200', 'Product Updated')
+  @Response('404', 'Product Not Found')
+  public async editProduct(
+    @Body() product: Product,
+    @Path() productId: UUID
+  ): Promise<Product | undefined> {
+    // FIXME: Should ownership be checked in the microservice?
+    const updatedProduct = await new ProductService().edit(productId, product);
+    if (!updatedProduct) {
+      this.setStatus(404);
+    }
+    return updatedProduct;
+  }
+
+  @Put('{productId}/setActiveStatus')
+  @SuccessResponse('200', 'Product Status Updated')
+  @Response('404', 'Product Not Found')
+  public async setActiveStatus(
+    @Path() productId: UUID,
+    @Query() active: boolean
+  ): Promise<Product | undefined> {
+    if (active) {
+      return await new ProductService().activate(productId);
+    } else {
+      return await new ProductService().deactivate(productId);
+    }
   }
 }
--- ProductService/src/product/productService.ts
+++ ProductService/src/product/productService.ts
@@ -1,13 +1,44 @@
-import { UUID } from "src/types";
-import { NewProduct, Product} from ".";
+import {UUID} from 'src/types';
+import {NewProduct, Product} from '.';
 import {pool} from '../db';
 export class ProductService {
-  public async create(product: NewProduct, vendor_id?: UUID): Promise<Product|undefined>{
+  public async exists(productId: UUID, vendorId?: UUID) {
+    let select = `SELECT * FROM product WHERE id = $1::UUID`;
+    const values = [productId];
+
+    if (vendorId) {
+      select += ` AND vendor_id = $${values.length + 1}::UUID`;
+      values.push(vendorId);
+    }
+
+    const query = {
+      text: select,
+      values,
+    };
+
+    const {rows} = await pool.query(query);
+    return rows[0];
+  }
+
+  public async isActive(productId: UUID): Promise<boolean> {
+    const select = `SELECT active FROM product WHERE id = $1::UUID`;
+    const query = {
+      text: select,
+      values: [productId],
+    };
+    const {rows} = await pool.query(query);
+    return rows[0].active;
+  }
+
+  public async create(
+    product: NewProduct,
+    vendor_id?: UUID
+  ): Promise<Product | undefined> {
     if (vendor_id) {
       const insert = `INSERT INTO product(vendor_id, data) VALUES ($1::UUID, jsonb_build_object('name', $2::TEXT, 'price', $3::TEXT)) RETURNING *`;
       const query = {
         text: insert,
-        values: [`${vendor_id}`, `${product.name}`, `${product.price}`]
+        values: [`${vendor_id}`, `${product.name}`, `${product.price}`],
       };
       const {rows} = await pool.query(query);
       return rows[0];
@@ -15,16 +46,103 @@ export class ProductService {
     return undefined;
   }
 
-  public async disable(id: UUID, vendor_id?: UUID): Promise<Product|undefined>{
-    if (vendor_id) {
-      const insert = `UPDATE product SET active = false WHERE id = $1::UUID AND vendor_id = $2::UUID RETURNING *`;
-      const query = {
-        text: insert,
-        values: [`${id}`, `${vendor_id}`]
-      };
-      const {rows} = await pool.query(query);
-      return rows[0];
+  public async edit(
+    productId: UUID,
+    product: Product
+  ): Promise<Product | undefined> {
+    const update = `UPDATE product SET data = jsonb_build_object('name', $1::TEXT, 'price', $2::TEXT) WHERE id = $3::UUID RETURNING *`;
+    const query = {
+      text: update,
+      values: [`${product.data.name}`, `${product.data.price}`, `${productId}`],
+    };
+    const {rows} = await pool.query(query);
+    return rows[0];
+  }
+
+  public async activate(
+    productId: UUID
+  ): Promise<Product | undefined> {
+    const update = `UPDATE product SET active = true WHERE id = $1::UUID RETURNING *`;
+    const query = {
+      text: update,
+      values: [`${productId}`],
+    };
+    const {rows} = await pool.query(query);
+    return rows[0];
+  }
+
+  public async deactivate(
+    productId: UUID
+  ): Promise<Product | undefined> {
+    const update = `UPDATE product SET active = false WHERE id = $1::UUID RETURNING *`;
+    const query = {
+      text: update,
+      values: [`${productId}`],
+    };
+    const {rows} = await pool.query(query);
+    return rows[0];
+  }
+
+  public async getAll(
+    vendorId?: UUID,
+    active?: boolean,
+    page: number = 1,
+    pageSize: number = 25,
+    search?: string,
+    orderBy: string = 'posted_at',
+    descending?: boolean
+  ): Promise<Product[]> {
+    // Base query
+    let select = `SELECT * FROM product`;
+    const values = [];
+    const conditions = [];
+
+    // Add conditions for each filter
+    if (vendorId) {
+      conditions.push(`vendor_id = $${values.length + 1}`);
+      values.push(vendorId);
     }
-    return undefined;
+    if (active) {
+      conditions.push(`active = $${values.length + 1}`);
+      values.push(active);
+    }
+    if (search) {
+      conditions.push(`data->>'name' ILIKE $${values.length + 1}`);
+      values.push(`%${search}%`);
+    }
+
+    // Append conditions to query
+    if (conditions.length) {
+      select += ` WHERE ${conditions.join(' AND ')}`;
+    }
+
+    // Order by valid columns
+    const validColumns = ['name', 'price', 'postedAt'];
+    if (orderBy && validColumns.includes(orderBy)) {
+      select += ` ORDER BY data->>'${orderBy}' ${descending ? 'DESC' : 'ASC'}`;
+    }
+
+    // Add limit and offset for pagination
+    select += ` LIMIT $${values.length + 1} OFFSET $${values.length + 2}`;
+    values.push(pageSize, (page - 1) * pageSize);
+
+    const query = {
+      text: select,
+      values,
+    };
+
+    const {rows} = await pool.query(query);
+    const products = rows.map((row) => row);
+    return products;
+  }
+
+  public async getOne(productId: UUID): Promise<Product | undefined> {
+    const select = `SELECT * FROM product WHERE id = $1::UUID`;
+    const query = {
+      text: select,
+      values: [`${productId}`],
+    };
+    const {rows} = await pool.query(query);
+    return rows[0];
   }
 }
