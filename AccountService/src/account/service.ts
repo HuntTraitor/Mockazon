8ca66fd945ee16de720bd630fe8abfd82718a864
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-05 05:22:41 -0700

    renamed account service subfolder 'user' to 'account' #79

--- AccountService/src/account/service.ts
+++ AccountService/src/account/service.ts
@@ -4,7 +4,7 @@ import { pool } from "../db";
 export class UserService {
   public async getAll(): Promise<User[]> {
     const query = {
-      text: "SELECT * FROM user_account",
+      text: "SELECT id FROM user_account",
     };
 
     const { rows } = await pool.query(query);
