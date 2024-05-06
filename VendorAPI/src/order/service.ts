2b7a69a8dc3d9a9615adb5d2ea32747222f1904e
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-05-06 15:44:37 -0700

    #95 connected microservice to vendorAPI

--- VendorAPI/src/order/service.ts
+++ VendorAPI/src/order/service.ts
@@ -1,3 +1,34 @@
 /**
  * This service must communicate with the OrderService
  */
+import { UUID } from '../types';
+import type { NewOrder, Order } from './index';
+
+export class OrderService {
+  async create(order: NewOrder, vendorId?: UUID): Promise<Order> {
+    return new Promise((resolve, reject) => {
+      fetch(`http://${process.env.MICROSERVICE_URL || 'localhost'}:3012/api/v0/vendororder?vendorId=${vendorId}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(order),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+        .then(res => {
+          if (!res.ok) {
+            throw res
+          }
+          return res.json()
+        })
+        .then(authenticated => {
+          resolve(authenticated)
+        })
+        .catch(err => {
+          console.log(err)
+          reject(err)
+        })
+    })
+  }
+}
