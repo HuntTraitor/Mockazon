1fbe95bf9ad0d25fd62b4bbe0bb053da9612bdd5
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-05 17:39:19 -0700

    Added endpoints for Account microservice [get, suspend, resume] accounts #87

--- AccountService/src/account/controller.ts
+++ AccountService/src/account/controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get, Route } from "tsoa";
+import { Controller, Get, Put, Route } from "tsoa";
 
-import { User } from ".";
+import { User, UUID } from "../types";
 import { AccountService } from "./service";
 
 @Route("account")
 export class AccountController extends Controller {
+  // fetch all exsiting user accounts
   @Get("")
   public async getAll(): Promise<User[]> {
     return new AccountService().getAll();
+  }  
+
+  // suspend an existing account
+  @Put("{id}/suspend")
+  public async suspend(id: UUID): Promise<void> {
+    console.log(id);
+    return new AccountService().suspend(id);
+  }
+
+  // resume a suspended account
+  @Put("{id}/resume")
+  public async resume(id: UUID): Promise<void> {
+    console.log(id);
+    return new AccountService().resume(id);
   }
 }
--- AccountService/src/account/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Email, UUID } from "../types";
-
-export interface User {
-  id: UUID;
-  email: Email;
-  name: string;
-  username: string;
-  role: string;
-}
