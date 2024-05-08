a6822e6fac1694334bff94850a0aed1de6595cc3
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-05-07 20:02:17 -0700

    Update getOneOrder.test.ts

--- OrderService/test/getOneOrder.test.ts
+++ OrderService/test/getOneOrder.test.ts
@@ -1,75 +1,44 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import supertest from 'supertest';
-import * as http from 'http';
+import { server } from './helper';
+import { NewOrder } from '../src/order';
+import { randomUUID } from 'crypto';
+import { validateOrder, validateNewOrder } from './helper';
 
-import * as db from './db';
-import app from '../src/app';
-
-let server: http.Server<
-  typeof http.IncomingMessage,
-  typeof http.ServerResponse
->;
-
-beforeEach(async () => {
-  server = http.createServer(app);
-  server.listen();
-  await db.reset();
-});
-
-afterAll(done => {
-  db.shutdown();
-  server.close(done);
-});
-
-const productId = '3f2687d0-d115-421f-ae23-ac572005a791';
-const accountId = '1ed8ca72-2027-4d19-ba6b-e6be75ab7d8b';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const mockOrder = {
-  product_id: productId,
-  account_id: accountId,
-  purchaseDate: new Date().toISOString(),
-  quantity: '2',
+const postOrder = async (): Promise<string | undefined> => {
+  let orderId;
+  const newOrder: NewOrder = {
+    product_id: randomUUID(),
+    shopper_id: randomUUID(),
+    quantity: '2',
+  };
+  validateNewOrder(newOrder);
+  await supertest(server)
+    .post(`/api/v0/order`)
+    .send(newOrder)
+    .query({ vendorId: randomUUID() })
+    .expect(201)
+    .then(res => {
+      validateOrder(res.body);
+      orderId = res.body.id;
+    });
+  return orderId;
 };
 
-test('temp', async () => {
-  expect(true).toBe(true);
+test('Gets successful order 200', async () => {
+  const orderId = await postOrder();
+  await supertest(server)
+    .get(`/api/v0/order/${orderId}`)
+    .expect(200)
+    .then(res => {
+      validateOrder(res.body);
+    });
 });
 
-// const postOrder = async (): Promise<string | undefined> => {
-//   let orderId;
-//   await supertest(server)
-//     .post(`/api/v0/order`)
-//     .send(mockOrder)
-//     .expect(201)
-//     .then(res => {
-//       orderId = res.body.id;
-//     });
-//   return orderId;
-// };
+test('Gets order not found 404', async () => {
+  const orderId = crypto.randomUUID();
+  await supertest(server).get(`/api/v0/order/${orderId}`).expect(404);
+});
 
-// test('Gets successful order 200', async () => {
-//   const orderId = await postOrder();
-//   await supertest(server)
-//     .get(`/api/v0/order/${orderId}`)
-//     .expect(200)
-//     .then(res => {
-//       expect(res.body).toBeDefined();
-//       expect(res.body.id).toBe(orderId);
-//       expect(res.body.product_id).toBe(productId);
-//       expect(res.body.account_id).toBe(accountId);
-//       expect(res.body.data).toBeDefined();
-//       expect(res.body.data.purchaseDate).toBeDefined();
-//       expect(res.body.data.quantity).toBe('2');
-//       expect(res.body.data.delivered).toBeFalsy();
-//       expect(res.body.data.received).toBeFalsy();
-//     });
-// });
-
-// test('Gets order not found 404', async () => {
-//   const orderId = crypto.randomUUID();
-//   await supertest(server).get(`/api/v0/order/${orderId}`).expect(404);
-// });
-
-// test('Gets order bad orderId 400', async () => {
-//   await supertest(server).get(`/api/v0/order/badid`).expect(400);
-// });
+test('Gets order bad orderId 400', async () => {
+  await supertest(server).get(`/api/v0/order/badid`).expect(400);
+});
