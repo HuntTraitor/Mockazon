f0e9c39999eebf3396e1008e40208a4c09cbc99a
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-05-08 00:13:15 -0700

    #87 added hunter as an admin

--- AccountService/sql/data.sql
+++ AccountService/sql/data.sql
@@ -5,6 +5,7 @@
 INSERT INTO account (data) VALUES (jsonb_build_object('email', 'addelros@ucsc.edu', 'name', 'Alfonso Del Rosario', 'username', 'addelros', 'role', 'vendor'));
 INSERT INTO account (data) VALUES (jsonb_build_object('email', 'evmetcal@ucsc.edu', 'name', 'Evan Metcalf', 'username', 'evmetcal', 'role', 'shopper'));
 INSERT INTO account (data) VALUES (jsonb_build_object('email', 'lteixeir@ucsc.edu', 'name', 'Lukas Teixeira Döpcke', 'username', 'lteixeir', 'role', 'admin'));
+INSERT INTO account (data) VALUES (jsonb_build_object('email', 'htratar@ucsc.edu', 'name', 'Hunter Tratar', 'username', 'huntertratar', 'role', 'admin'));
 
 -- Approve / Reject Test Data --
 INSERT INTO account (id, data) VALUES ('ce1186e7-a1f2-4bff-bbfc-b33641fe5ecd', jsonb_build_object('email', 'approve@email.com', 'name', 'Approve Test', 'username', 'approve', 'role', 'shopper', 'suspended', false)); 
