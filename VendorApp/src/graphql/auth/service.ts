36045c242764fb6a47cbcdcc9ca074e46ca4e546
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-05-09 01:57:22 -0700

    #31 commented out unnecessary login call for now

--- VendorApp/src/graphql/auth/service.ts
+++ VendorApp/src/graphql/auth/service.ts
@@ -19,30 +19,30 @@ export class AuthService {
 
 
   }
-  public async login(credentials: Credentials): Promise<Authenticated>  {
-    return new Promise((resolve, reject) => {
-      fetch('http://localhost:3011/api/v0/authenticate', {
-        method: 'POST',
-        body: JSON.stringify(credentials),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw res
-          }
-          return res.json()
-        })
-        .then((authenticated) => {
-          resolve(authenticated)
-        })
-        .catch((err) => {
-          console.log(err)
-          reject(new Error("Unauthorised"))
-        });
-    });
-  }
+  // public async login(credentials: Credentials): Promise<Authenticated>  {
+  //   return new Promise((resolve, reject) => {
+  //     fetch('http://localhost:3011/api/v0/authenticate', {
+  //       method: 'POST',
+  //       body: JSON.stringify(credentials),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     })
+  //       .then((res) => {
+  //         if (!res.ok) {
+  //           throw res
+  //         }
+  //         return res.json()
+  //       })
+  //       .then((authenticated) => {
+  //         resolve(authenticated)
+  //       })
+  //       .catch((err) => {
+  //         console.log(err)
+  //         reject(new Error("Unauthorised"))
+  //       });
+  //   });
+  // }
 
 
   public async check(authHeader?: string, scopes?: string[]): Promise<SessionUser> {
