e94ab58d393780a1a3d8752bdd3600146b7d5f51
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-05-09 01:56:12 -0700

    #31 Graphql Boilerplate added to VendorApp

---
+++ VendorApp/src/graphql/auth/resolver.ts
@@ -0,0 +1,20 @@
+import {Args, Query, Resolver } from "type-graphql"
+import { Authenticated, Credentials, Message, SignupCredentials } from "./schema";
+import { AuthService } from "./service";
+
+@Resolver()
+export class AuthResolver {
+  @Query(() => Message)
+  async signup(
+    @Args() credentials: SignupCredentials
+  ): Promise<Message> {
+    return new AuthService().signup(credentials)
+  }
+
+  // @Query(() => Authenticated)
+  // async login(
+  //   @Args() credentials: Credentials,
+  // ): Promise<Authenticated> {
+  //   return new AuthService().login(credentials)
+  // }
+}
---
+++ VendorApp/src/graphql/auth/schema.ts
@@ -0,0 +1,36 @@
+import {Field, ObjectType, ArgsType} from "type-graphql"
+import {Matches} from "class-validator"
+
+@ArgsType()
+export class Credentials {
+  @Field()
+  @Matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    email!: string
+  @Field()
+    password!: string
+}
+
+@ArgsType()
+export class SignupCredentials {
+  @Field()
+    name!: string
+  @Field()
+  @Matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    email!: string
+  @Field()
+    password!: string
+}
+
+@ObjectType()
+export class Authenticated {
+  @Field()
+    name!: string
+  @Field()
+    accessToken!: string
+}
+
+@ObjectType()
+export class Message {
+  @Field()
+    content!: string
+}
