d2a9d2f0dae2bde43354872da5fc1140f32ad37f
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-10 15:15:06 -0700

    full admin component coverage #79

--- Admin/test/views/HomePage/Home.test.tsx
+++ Admin/test/views/HomePage/Home.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import { Home } from '../../../src/views/HomePage/Home';
 import { LoginContext } from '@/contexts/Login';
+import { PageContext } from '@/contexts/PageContext';
 
 it('Renders...Access Key Exists', async () => {
   const accessToken = 'some old token';
@@ -14,6 +15,40 @@ it('Renders...Access Key Exists', async () => {
     </LoginContext.Provider>
   );
   expect(screen.getAllByText('Users').length).not.toBe(0);
+});
+
+it('Renders Correct Page (Users)', async () => {
+  const accessToken = 'some old token';
+  const setAccessToken = () => {};
+  const id = 'some id';
+  const setId = () => {};
+  const page = 'Users';
+  const setPage = () => {};
+  render(
+    <LoginContext.Provider value={{ id, setId, accessToken, setAccessToken }}>
+      <PageContext.Provider value={{ page, setPage }}>
+        <Home />
+      </PageContext.Provider>
+    </LoginContext.Provider>
+  );
+  expect(screen.getAllByText('Users').length).not.toBe(0);
+});
+
+it('Renders Correct Page (Requests)', async () => {
+  const accessToken = 'some old token';
+  const setAccessToken = () => {};
+  const id = 'some id';
+  const setId = () => {};
+  const page = 'Requests';
+  const setPage = () => {};
+  render(
+    <LoginContext.Provider value={{ id, setId, accessToken, setAccessToken }}>
+      <PageContext.Provider value={{ page, setPage }}>
+        <Home />
+      </PageContext.Provider>
+    </LoginContext.Provider>
+  );
+  expect(screen.getAllByText('Requests').length).not.toBe(0);
 });
 
 it('Does not Render...Empty Access Key', async () => {
