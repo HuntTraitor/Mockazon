53bbd418c81fbc3693e17f8a603e372512f76041
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-05-10 17:05:45 -0700

    #31 Added a default theme to Vendor App

--- VendorApp/src/views/App.tsx
+++ VendorApp/src/views/App.tsx
@@ -1,9 +1,28 @@
 import { Fragment } from 'react';
 import { Home } from './HomePage/Home';
-export const App = () => {
+import { LoginProvider } from '@/context/Login';
+import {SignupForm} from './Signup/SignupForm'
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffDA00',
+    },
+    secondary: {
+      main: '#1976d2',
+    },
+  },
+});
+
+
+export function App() {
   return (
-    <Fragment>
-      <Home />
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <LoginProvider>
+        <SignupForm />
+        {/* <Home /> */}
+      </LoginProvider>
+    </ThemeProvider>
   );
 };
