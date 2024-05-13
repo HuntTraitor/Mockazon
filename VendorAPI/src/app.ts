16ceade2731fcbc10197809e40ab6d63f16511be
Luke <22577678+Iuke121@users.noreply.github.com>
2024-05-13 09:15:25 -0700

    Change default doc route for vendorapi #2

--- VendorAPI/src/app.ts
+++ VendorAPI/src/app.ts
@@ -15,15 +15,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  '/docs',
-  swaggerUi.serve,
-  async (_req: ExRequest, res: ExResponse) => {
-    return res.send(
-      swaggerUi.generateHTML(await import('../build/swagger.json'))
-    );
-  }
-);
+app.use('/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import('../build/swagger.json'))
+  );
+});
 
 const router = Router();
 RegisterRoutes(router);
