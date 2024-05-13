600216cbe38bf91e9a7fe068a20e7612dbbaae00
Luke <22577678+Iuke121@users.noreply.github.com>
2024-05-13 09:35:44 -0700

    Fix docs route #2

--- VendorAPI/src/app.ts
+++ VendorAPI/src/app.ts
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+app.use('/v0/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
   return res.send(
     swaggerUi.generateHTML(await import('../build/swagger.json'))
   );
