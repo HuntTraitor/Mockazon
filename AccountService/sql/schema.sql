63ec2e97478b81113f23c54659a25920294b2c01
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-13 11:36:54 -0700

    schema update for account service (admin) #119

--- AccountService/sql/schema.sql
+++ AccountService/sql/schema.sql
@@ -1,4 +1,11 @@
 DROP TABLE IF EXISTS request;
+DROP TABLE IF EXISTS vendor;
+DROP TABLE IF EXISTS shopper;
+DROP TABLE IF EXISTS administrator;
 DROP TABLE IF EXISTS account;
+
 CREATE TABLE account(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
+CREATE TABLE administrator(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
+CREATE TABLE shopper(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
+CREATE TABLE vendor(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
 CREATE TABLE request(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), account_id UUID, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
