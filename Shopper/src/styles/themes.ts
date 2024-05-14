a29af252256a0c8041843b4b84bddfc63e0e6088
Luke <22577678+Iuke121@users.noreply.github.com>
2024-05-13 17:02:14 -0700

    Remote palette for now #132

--- Shopper/src/styles/themes.ts
+++ Shopper/src/styles/themes.ts
@@ -1,14 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 
 export const mainTheme = createTheme({
-  palette: {
-    primary: {
-      main: '#ffDA00',
-    },
-    secondary: {
-      main: '#1976d2',
-    },
-  },
   typography: {
     fontFamily: 'Open Sans, sans-serif',
   },
