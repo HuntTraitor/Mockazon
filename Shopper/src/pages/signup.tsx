0a21409d4fafa00b1cd450a95c07afd17a4ba299
Luke <22577678+Iuke121@users.noreply.github.com>
2024-05-13 18:02:31 -0700

    Visual overhaul of sign up #132

--- Shopper/src/pages/signup.tsx
+++ Shopper/src/pages/signup.tsx
@@ -1,5 +1,12 @@
 import { useContext, useEffect, useState } from 'react';
-import { Box, Container, Grid, Paper, Typography } from '@mui/material';
+import {
+  Box,
+  Divider,
+  Paper,
+  TextField,
+  Typography,
+  ThemeProvider,
+} from '@mui/material';
 import {
   CredentialResponse,
   GoogleLogin,
@@ -7,12 +14,15 @@ import {
 } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { LoggedInContext } from '@/contexts/LoggedInUserContext';
-import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import LanguageSwitcher from '@/views/LanguageSwitcher';
 import { useRouter } from 'next/router';
+import { mainTheme } from '@/styles/themes';
+import styles from '@/styles/Signup.module.css';
+import Image from 'next/image';
+import { Button } from '@mui/material';
+import Link from 'next/link';
 
 const namespaces = ['common', 'login', 'signup', 'products'];
 export const getServerSideProps: GetServerSideProps = async context => {
@@ -23,18 +33,71 @@ export const getServerSideProps: GetServerSideProps = async context => {
   };
 };
 
-// based this login screen off of lukas's cse 115 project
 const Signup = () => {
   const [error, setError] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const { accessToken, setAccessToken } = useContext(LoggedInContext);
-  const { t } = useTranslation('signup');
+  const { t, i18n } = useTranslation(['common', 'signup']);
   const router = useRouter();
 
   useEffect(() => {
     localStorage.removeItem('user');
   }, []);
 
-  const handleSuccess = async (credentialResponse: CredentialResponse) => {
+  const handleSignUp = async () => {
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+    try {
+      const query = {
+        query: `mutation SignUp {
+    signUp(name: "${name}", password: "${password}") {
+      id
+      name
+      email
+      role
+      accessToken
+    }
+  }`,
+      };
+      const response = await fetch('/api/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(query),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data.errors && data.errors.length > 0) {
+          if (data.errors[0].message === 'Duplicate account') {
+            console.error('Duplicate account:', error);
+            setError('Duplicate account');
+            return;
+          }
+        } else {
+          console.error('Error logging in:', error);
+          setError('Login failed');
+        }
+        localStorage.setItem('user', JSON.stringify(data.data.signUp));
+        setAccessToken(data.data.signUp.accessToken);
+        await router.push('/');
+      } else {
+        console.error('Error logging in:', error);
+        setError('Login failed');
+        return;
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setError('Login failed');
+    }
+  };
+
+  const handleGoogleSuccess = async (
+    credentialResponse: CredentialResponse
+  ) => {
     try {
       const decoded: { sub: string; email: string; name: string } = jwtDecode(
         credentialResponse?.credential as string
@@ -72,7 +135,7 @@ const Signup = () => {
         }
         localStorage.setItem('user', JSON.stringify(data.data.signUp));
         setAccessToken(data.data.signUp.accessToken);
-        await router.push('/products');
+        await router.push('/');
       } else {
         console.error('Error logging in:', error);
         setError('Login failed');
@@ -83,73 +146,107 @@ const Signup = () => {
       setError('Login failed');
     }
   };
+
   const OAUTH_CLIENT_ID =
     '655989276717-5viil57sbom25s2804kadpdt3kiaa4on.apps.googleusercontent.com';
 
-  return !accessToken ? (
-    <GoogleOAuthProvider clientId={OAUTH_CLIENT_ID}>
-      <LanguageSwitcher />
-      <Container maxWidth="sm">
-        <Grid
-          container
-          spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          style={{ minHeight: '70vh' }}
-        >
-          <Grid item xs={12} textAlign="center">
-            <Typography
-              aria-label={'title'}
-              variant="h4"
-              component="h1"
-              gutterBottom
-            >
-              {t('title')}
+  if (accessToken) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <Box className={styles.signUpContainer}>
+        <Image
+          src="/mockazon_logo_white.png"
+          alt="logo"
+          width={180}
+          height={100}
+          className={styles.logo}
+        />
+        <Paper elevation={3} className={styles.signUpForm}>
+          <div className={styles.titleContainer}>
+            <Typography variant="h4" className={styles.title}>
+              {t('signup:title')}
             </Typography>
-            <Link
-              style={{ color: 'blue' }}
-              aria-label={'sub-title'}
-              href={'#'}
-              onClick={() => router.push('/login')}
-            >
-              <Typography variant="h6" component="h6" gutterBottom>
-                {t('sub-title')}
-              </Typography>
-            </Link>
-          </Grid>
-          <Grid item xs={12} textAlign="center">
-            <Paper
-              elevation={3}
-              sx={{
-                padding: 4,
-              }}
-            >
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <Typography variant="h4" gutterBottom>
-                  {t('prompt')}
-                </Typography>
-                {error && (
-                  <Typography variant="body1" color="error">
-                    {error}
-                  </Typography>
-                )}
-                <GoogleLogin onSuccess={handleSuccess} />
-              </Box>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </GoogleOAuthProvider>
-  ) : (
-    <></>
+          </div>
+          <TextField
+            className={styles.nameInput}
+            label={t('signup:name')}
+            value={name}
+            size="small"
+            onChange={event => setName(event.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            className={styles.emailInput}
+            label={t('signup:email')}
+            value={email}
+            size="small"
+            onChange={event => setEmail(event.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            className={styles.passwordInput}
+            label={t('signup:password')}
+            type="password"
+            size="small"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            className={styles.confirmPasswordInput}
+            label={t('signup:confirmPassword')}
+            type="password"
+            size="small"
+            value={confirmPassword}
+            onChange={event => setConfirmPassword(event.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            className={styles.signUpButton}
+            variant="contained"
+            color="primary"
+            onClick={handleSignUp}
+            fullWidth
+          >
+            {t('signup:signUpText')}
+          </Button>
+          <Divider className={styles.divider}>{t('common:or')}</Divider>
+          <GoogleOAuthProvider clientId={OAUTH_CLIENT_ID}>
+            <GoogleLogin
+              shape="rectangular"
+              width="350px"
+              context="signup"
+              text="signup_with"
+              locale={i18n.language == 'en' ? 'en-US' : 'es-US'}
+              onSuccess={handleGoogleSuccess}
+            />
+          </GoogleOAuthProvider>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            className={styles.agreementText}
+          >
+            {t('signup:agreement')}
+          </Typography>
+          <div className={styles.titleContainer}>
+            <Typography variant="body2" className={styles.alreadyHaveAccount}>
+              {t('signup:alreadyHaveAccount')}{' '}
+              <Link className={styles.signInButton} href="/login">
+                {t('common:signInText')}
+              </Link>
+            </Typography>
+          </div>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
-  // UI partly referenced from: https://chat.openai.com/share/ee28fc31-d9b8-4193-8cb5-1bb793b8c66d
 };
 
 export default Signup;
