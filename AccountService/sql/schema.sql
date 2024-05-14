f9d0ca356653bc4564205f0e76d9a6bf15ac3b17
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-14 15:32:54 -0700

    reformatted sql files to allow test scripts to run #143

--- AccountService/sql/schema.sql
+++ AccountService/sql/schema.sql
@@ -6,26 +6,10 @@ DROP TABLE IF EXISTS shopper;
 
 DROP TABLE IF EXISTS administrator;
 
-DROP TABLE IF EXISTS account; -- Will be removed --
-CREATE TABLE account(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb); -- Will be removed --
+CREATE TABLE administrator(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
 
-CREATE TABLE administrator(
-    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
-    data jsonb
-);
+CREATE TABLE shopper(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
 
-CREATE TABLE shopper(
-    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
-    data jsonb
-);
+CREATE TABLE vendor(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
 
-CREATE TABLE vendor(
-    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
-    data jsonb
-);
-
-CREATE TABLE request(
-    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data jsonb
-);
+CREATE TABLE request(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, data jsonb);
