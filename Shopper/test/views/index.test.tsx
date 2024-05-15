04ff0024eeddb32dbdfb387af77bd9eb74ea4c8b
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-05-14 17:39:45 -0700

    Update index.test.tsx

--- Shopper/test/views/index.test.tsx
+++ Shopper/test/views/index.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-
+import { AppContext } from '@/contexts/AppContext';
 import Index, { getServerSideProps } from '../../src/pages/index';
 
 // https://chat.openai.com/share/0c95f9af-f8b8-49a7-b104-efd929b218e7
@@ -25,8 +25,17 @@ jest.mock('@fontsource/open-sans', () => {
   return {};
 });
 
+const AppContextProps = {
+  backDropOpen: false,
+  setBackDropOpen: jest.fn(),
+};
+
 it('Renders', async () => {
-  render(<Index />);
+  render(
+    <AppContext.Provider value={AppContextProps}>
+      <Index />
+    </AppContext.Provider>
+  );
 });
 
 jest.mock('next-i18next/serverSideTranslations', () => ({
