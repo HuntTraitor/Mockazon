f0236d1a292909fe7e04e2bc0e5597f0c3701534
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-05-14 17:39:42 -0700

    Update LanguageSwitcher.test.tsx

--- Shopper/test/views/LanguageSwitcher.test.tsx
+++ Shopper/test/views/LanguageSwitcher.test.tsx
@@ -1,6 +1,7 @@
 import LanguageSwitcher from '@/views/LanguageSwitcher';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
+import { AppContext } from '@/contexts/AppContext';
 
 const pushMock = jest.fn();
 let locale = 'en';
@@ -23,33 +24,57 @@ jest.mock('next/router', () => ({
   }),
 }));
 
+const AppContextProps = {
+  backDropOpen: false,
+  setBackDropOpen: jest.fn(),
+};
+
 describe('Language Switcher', () => {
   it('Renders successfully', async () => {
-    render(<LanguageSwitcher />);
+    render(
+      <AppContext.Provider value={AppContextProps}>
+        <LanguageSwitcher />
+      </AppContext.Provider>
+    );
     await screen.findByText('EN');
   });
 
   it('Clicking the language switcher opens the language choices', async () => {
-    render(<LanguageSwitcher />);
-    fireEvent.click(screen.getByLabelText('Language Container'));
+    render(
+      <AppContext.Provider value={AppContextProps}>
+        <LanguageSwitcher />
+      </AppContext.Provider>
+    );
+    fireEvent.mouseEnter(screen.getByLabelText('Language Box'));
+    fireEvent.click(screen.getByLabelText('Language Box'));
     await screen.findByText('English');
     await screen.findByText('Español');
   });
 
   it('Selecting a new language changes the locale', async () => {
-    render(<LanguageSwitcher />);
+    render(
+      <AppContext.Provider value={AppContextProps}>
+        <LanguageSwitcher />
+      </AppContext.Provider>
+    );
     pushMock.mockImplementation(() => Promise.resolve());
-    fireEvent.click(screen.getByLabelText('Language Container'));
+    fireEvent.mouseEnter(screen.getByLabelText('Language Box'));
+    fireEvent.click(screen.getByLabelText('Language Box'));
     fireEvent.click(screen.getByText('Español'));
     expect(pushMock).toHaveBeenCalledWith('/', '/', { locale: 'es' });
 
-    fireEvent.click(screen.getByLabelText('Language Container'));
+    fireEvent.mouseEnter(screen.getByLabelText('Language Box'));
+    fireEvent.click(screen.getByLabelText('Language Box'));
     fireEvent.click(screen.getByText('English'));
     expect(pushMock).toHaveBeenCalledWith('/', '/', { locale: 'en' });
   });
 
   it('Closes the language choices when the mouse leaves', async () => {
-    render(<LanguageSwitcher />);
+    render(
+      <AppContext.Provider value={AppContextProps}>
+        <LanguageSwitcher />
+      </AppContext.Provider>
+    );
     fireEvent.mouseEnter(screen.getByLabelText('Language Container'));
     fireEvent.mouseLeave(screen.getByLabelText('Language Container'));
 
@@ -59,19 +84,27 @@ describe('Language Switcher', () => {
   });
 
   it('Does not close if mouse re-enters before timeout', async () => {
-    render(<LanguageSwitcher />);
+    render(
+      <AppContext.Provider value={AppContextProps}>
+        <LanguageSwitcher />
+      </AppContext.Provider>
+    );
     fireEvent.mouseEnter(screen.getByLabelText('Language Container'));
     fireEvent.mouseLeave(screen.getByLabelText('Language Container'));
-    fireEvent.mouseEnter(screen.getByLabelText('Language Container'));
+    fireEvent.mouseEnter(screen.getByLabelText('Language Box'));
 
     await waitFor(() => {
       expect(screen.queryByText('English')).not.toBeNull();
     });
   });
 
-  it('Renders in spanish for the spanish locale', async () => {
+  it('Renders in Spanish for the Spanish locale', async () => {
     locale = 'es';
-    render(<LanguageSwitcher />);
+    render(
+      <AppContext.Provider value={AppContextProps}>
+        <LanguageSwitcher />
+      </AppContext.Provider>
+    );
     await screen.findByText('ES');
   });
 });
