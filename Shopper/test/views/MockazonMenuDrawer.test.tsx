5cd990b84f7ad42989092bdbb304dad97bcf99e5
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-05-19 12:34:59 -0700

    Create MockazonMenuDrawer.test.tsx

---
+++ Shopper/test/views/MockazonMenuDrawer.test.tsx
@@ -0,0 +1,27 @@
+import MockazonMenuDrawer from '@/views/MockazonMenuDrawer';
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { AppContext } from '@/contexts/AppContext';
+
+test('triggers onClose when backdrop is clicked', async () => {
+  const setMockazonMenuDrawerOpen = jest.fn();
+  const AppContextProps = {
+    backDropOpen: false,
+    setBackDropOpen: jest.fn(),
+    mockazonMenuDrawerOpen: true,  // Initially open the drawer
+    setMockazonMenuDrawerOpen,
+  };
+  
+  render(
+    <AppContext.Provider value={AppContextProps}>
+      <MockazonMenuDrawer />
+    </AppContext.Provider>
+  );
+
+  const backdrop = document.querySelector('.MuiBackdrop-root');
+  if (backdrop) {
+    fireEvent.click(backdrop);
+  }
+  
+  expect(setMockazonMenuDrawerOpen).toHaveBeenCalledWith(false);
+});
