65b6eb25ade9315b9cbbb13b9ab353d61dc3f940
Luke <22577678+Iuke121@users.noreply.github.com>
2024-05-20 12:54:30 -0700

    Redirect docs page #2

--- VendorAPI/src/app.ts
+++ VendorAPI/src/app.ts
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/vendorapi/v0/docs', function(req, res) {
+  res.redirect('/vendorapi/v0/docs/');
+});
+
 app.use(
   '/v0/docs/',
   swaggerUi.serve,
