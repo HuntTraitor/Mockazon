a00345afca71a773bfbaecfa5720513072992b7e
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-20 19:21:20 -0700

    General frontend tests for Admin Request Component #118

--- Admin/test/views/HomePage/AdminRequests.test.tsx
+++ Admin/test/views/HomePage/AdminRequests.test.tsx
@@ -1,6 +1,52 @@
 import { render, screen } from '@testing-library/react';
-
+import { graphql, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 import { AdminRequests } from '../../../src/views/HomePage/AdminRequests';
+
+let returnError = false;
+
+jest.mock('next/config', () => () => ({
+  publicRuntimeConfig: { basePath: '' },
+}));
+
+const handlers = [
+  graphql.query('GetRequests', ({ query /*variables*/ }) => {
+    console.log(query);
+    if (returnError) {
+      return HttpResponse.json({
+        errors: [{ message: 'Some Error' }],
+      });
+    }
+    return HttpResponse.json({
+      data: {
+        request: [
+          {
+            id: '81c689b1-b7a7-4100-8b2d-309908b444f5',
+            email: 'test1@email.com',
+            name: 'test account 1',
+            username: 'testaccount1',
+            role: 'test',
+            suspended: false,
+          },
+        ],
+      },
+    });
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+it('Renders', async () => {
+  render(<AdminRequests />);
+  await screen.findByText('Requests', {
+    exact: false,
+  });
+});
+
 
 it('Renders', async () => {
   render(<AdminRequests />);
@@ -13,19 +59,32 @@ it('Renders table', async () => {
   render(<AdminRequests />);
   await screen.findByText('Name');
   await screen.findByText('Email');
-  await screen.findByText('Username');
-  await screen.findByText('Request Type');
   await screen.findByText('Action');
 });
 
-it('Approve and Delete buttons for request with ID 1 are clickable', async () => {
+it('Approve button for request with ID 1 is clickable', async () => {
   render(<AdminRequests />);
-  const approveButtonForRequest1 =
-    await screen.findByTestId('approve-request-1');
-  const rejectButtonForRequest1 = await screen.findByTestId('reject-request-1');
+  const approveButtonForRequest1 = await screen.findByTestId(
+    'approve-request-81c689b1-b7a7-4100-8b2d-309908b444f5'
+  );
+
+  approveButtonForRequest1.click();
 
   expect(approveButtonForRequest1).not.toBeNull();
-  expect(rejectButtonForRequest1).not.toBeNull();
-  approveButtonForRequest1.click();
+});
+
+it('Reject button for request with ID 1 is clickable', async () => {
+  render(<AdminRequests />);
+  const rejectButtonForRequest1 = await screen.findByTestId(
+    'reject-request-81c689b1-b7a7-4100-8b2d-309908b444f5'
+  );
+
   rejectButtonForRequest1.click();
+  
+  expect(rejectButtonForRequest1).not.toBeNull();
+});
+
+it('Handles error', async () => {
+  returnError = true;
+  render(<AdminRequests />);
 });
