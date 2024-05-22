a42df23a783e22c02142e24beed27cc6b178510a
alfonsodelr <delrosarioluis22@gmail.com>
2024-05-21 20:58:51 -0700

    Added admin restriction to fetch all accounts graphQL endpoint #168

--- Admin/src/graphql/account/resolver.ts
+++ Admin/src/graphql/account/resolver.ts
@@ -1,11 +1,11 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'; //Authorized
+import { Arg, Mutation, Query, Resolver, Authorized } from 'type-graphql'; //Authorized
 
 import { Account } from './schema';
 import { AccountService } from './service';
 
 @Resolver()
 export class AccountResolver {
-  // @Authorized('member') // At some point we want to restrict access for this query to admin only
+  @Authorized("admin")
   @Query(() => [Account])
   async account(): Promise<Account[]> {
     return new AccountService().all();
