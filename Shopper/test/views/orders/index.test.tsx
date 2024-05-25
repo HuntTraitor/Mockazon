09d363525a0caefef8dd947ea0642f58aeb62712
tryles <trevorryles02@gmail.com>
2024-05-25 14:22:42 -0700

    Fix post merge issue

--- Shopper/test/views/orders/index.test.tsx
+++ Shopper/test/views/orders/index.test.tsx
@@ -1,3 +1,130 @@
-test('pass test', () => {
-    expect(true).toBe(true);
+
+import http from 'http';
+import { graphql, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+
+import requestHandler from '../../api/requestHandler';
+
+jest.mock('next/config', () => () => ({
+  publicRuntimeConfig: { basePath: '' },
+}));
+
+let server: http.Server<
+    typeof http.IncomingMessage,
+    typeof http.ServerResponse
+>;
+
+let errorInFetchProduct = false;
+let errorInShoppingCart = false;
+const errorInGraphQLShoppingCart = false;
+
+const handlers = [
+  graphql.query('GetShoppingCart', () => {
+    if (errorInShoppingCart) {
+      if (errorInGraphQLShoppingCart) {
+        return HttpResponse.json(
+            { errors: [{ message: 'error' }] },
+            { status: 200 }
+        );
+      }
+      return HttpResponse.json({}, { status: 400 });
+    } else {
+      return HttpResponse.json(
+          {
+            data: {
+              getShoppingCart: [
+                {
+                  id: '123',
+                  product_id: '123',
+                  shopper_id: '123',
+                  vendor_id: '123',
+                  data: {
+                    quantity: '3',
+                  },
+                },
+              ],
+            },
+          },
+          { status: 200 }
+      );
+    }
+  }),
+  graphql.query('GetProduct', () => {
+    if (errorInFetchProduct) {
+      return HttpResponse.json({}, { status: 400 });
+    } else {
+      return HttpResponse.json(
+          {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            id: '123',
+            data: {
+              getProduct: {
+                data: {
+                  brand: 'test',
+                  name: 'test name',
+                  rating: 'test',
+                  price: 1,
+                  deliveryDate: 'test',
+                  image: 'test',
+                },
+              },
+            },
+          },
+          { status: 200 }
+      );
+    }
+  }),
+];
+
+const microServices = setupServer(...handlers);
+
+beforeAll(async () => {
+  microServices.listen({ onUnhandledRequest: 'bypass' });
+  server = http.createServer(requestHandler);
+  server.listen();
 });
+
+beforeEach(() => {
+  microServices.resetHandlers();
+  errorInFetchProduct = false;
+  errorInShoppingCart = false;
+});
+
+afterAll(done => {
+  microServices.close();
+  server.close(done);
+});
+
+jest.mock('next-i18next/serverSideTranslations', () => ({
+  serverSideTranslations: jest.fn().mockReturnValue({
+    en: {
+      common: {
+        title: 'Mock Title',
+      },
+    },
+  }),
+}));
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    basePath: '',
+    pathname: '/',
+    query: {},
+    asPath: '/',
+    locale: 'en',
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    push: jest.fn(),
+    replace: jest.fn(),
+    reload: jest.fn(),
+    back: jest.fn(),
+    prefetch: jest.fn(),
+    beforePopState: jest.fn(),
+  }),
+}));
+
+it('Renders successfully', async () => {
+  expect(true).toBe(true);
+});
+
