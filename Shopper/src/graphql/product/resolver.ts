ac3c7eec70db5a3df5aec786e7f3153b911f937a
NaveFlactem <122710790+naveflactem@users.noreply.github.com>
2024-05-25 02:06:55 -0700

    add suggestions to graphql  #101

--- Shopper/src/graphql/product/resolver.ts
+++ Shopper/src/graphql/product/resolver.ts
@@ -27,9 +27,7 @@ export class ProductResolver {
   }
 
   @Query(() => [String])
-  async getSearchSuggestions(
-    @Arg('search', () => String) search: string
-  ): Promise<string[]> {
+  async getSearchSuggestions(@Arg('search', () => String) search: string): Promise<string[]> {
     return this.productService.getSearchSuggestions(search);
   }
 }
