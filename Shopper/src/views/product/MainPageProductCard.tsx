45ed1f0004d81a4aba1225b4dd81754bd6d505db
alfonsodelr <61682073+alfonsodelr@users.noreply.github.com>
2024-05-27 16:05:29 -0700

    Made product card component for main page #206

---
+++ Shopper/src/views/product/MainPageProductCard.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+import { Product } from '@/graphql/types';
+import { Link } from '@mui/material';
+import Price from './Price';
+import DeliveryText from './DeliveryText';
+import AddToCartButton from './AddToCartButton';
+import { useTranslation } from 'next-i18next';
+import styles from '@/styles/ProductCard.module.css';
+
+interface ProductProps {
+  product: Product;
+}
+
+export default function ProductCard({ product }: ProductProps) {
+  const { t } = useTranslation('viewProduct');
+  const price = product.data.price.toString();
+
+  return (
+    <Card
+      sx={{
+        width: 250,
+        height: 500,
+        boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
+        transition: 'all 0.3s ease',
+      }}
+    >
+      <Link href={`/products/${product.id}`} underline="none">
+        <Image
+          src={product.data.image}
+          alt={t('productImageAlt')}
+          width={250}
+          height={250}
+        />
+      </Link>
+      <CardContent>
+        <Link href={`/products/${product.id}`} underline="none" color="inherit">
+          <Typography
+            style={{
+              fontSize: '1rem',
+            }}
+            component="div"
+            className={styles.productName}
+          >
+            {product.data.name}
+          </Typography>
+        </Link>
+        <Price price={price} />
+        <Typography
+          variant="body1"
+          color="text.secondary"
+          className={styles.primeLogo}
+        >
+          <Image
+            src="/prime_logo.jpg"
+            alt="Prime logo"
+            width={70}
+            height={21.5}
+          />
+        </Typography>
+        <DeliveryText deliveryDate={product.data.deliveryDate} />
+        <AddToCartButton product={product} quantity={'1'} />
+      </CardContent>
+    </Card>
+  );
+}
