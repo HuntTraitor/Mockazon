e40a353f60efd83d3850e135a07bcc3cd195223d
tryles <trevorryles02@gmail.com>
2024-05-26 20:44:03 -0700

    Fix issue where time displaying in UTC format #210

--- Shopper/src/pages/cart.tsx
+++ Shopper/src/pages/cart.tsx
@@ -271,7 +271,7 @@ const Cart = ({ locale }: { locale: string }) => {
                     component="p"
                   >
                     {t('products:deliveryDate')}:{' '}
-                    {product.data.getProduct.data.deliveryDate}
+                    {new Intl.DateTimeFormat('en-US', { weekday: 'long', month: 'short', day: 'numeric' }).format(new Date(product.data.getProduct.data.deliveryDate as string))}
                   </Typography>
                   <Typography
                     aria-label={`quantity is ${product.quantity}`}
