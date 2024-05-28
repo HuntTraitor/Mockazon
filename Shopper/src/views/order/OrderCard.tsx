ddaadef2c00f2949d58c35159dde5c3860e29612
tryles <trevorryles02@gmail.com>
2024-05-28 11:43:17 -0700

    Fix total with quantity not displaying correctly on order

--- Shopper/src/views/order/OrderCard.tsx
+++ Shopper/src/views/order/OrderCard.tsx
@@ -272,7 +272,7 @@ export default function OrderCard({ order }: { order: Order }) {
                         variant="body2"
                         className={styles.productPrice}
                       >
-                        Total: ${Number(product.data.price).toFixed(2)}
+                        Total: ${(Number(product.data.price) * product.quantity).toFixed(2)}
                       </Typography>
                       <Box className={styles.actionButtons}>
                         <Button
