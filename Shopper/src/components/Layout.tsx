4c3f74bb0c06f0e7e5f96b561b81b0f95758b187
NaveFlactem <122710790+NaveFlactem@users.noreply.github.com>
2024-05-28 17:59:38 -0700

    apply account drawer to layout #221

--- Shopper/src/components/Layout.tsx
+++ Shopper/src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import AccountDrawer from '@/views/AccountDrawer';
 import TopNav from '@/views/TopNav';
 import React from 'react';
 
@@ -6,6 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <TopNav />
       {children}
+      <AccountDrawer />
     </>
   );
 }
