1a4531a8a3140757fd0cbabc45f724d0a370a2c9
alfonsodelr <61682073+alfonsodelr@users.noreply.github.com>
2024-05-30 10:56:21 -0700

    Added arrows for scrolling through ProductCarousel component #187

--- Shopper/src/views/ProductCarousel.tsx
+++ Shopper/src/views/ProductCarousel.tsx
@@ -1,9 +1,11 @@
 // ProductCarousel.tsx
-import React from 'react';
+import React, { useRef } from 'react';
 import { Box, Button, CardActionArea, Typography } from '@mui/material';
 import Image from 'next/image';
 import styles from '@/styles/MainPage.module.css';
 import { Product } from '@/graphql/types';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 interface ProductCarouselProps {
   products: Product[];
@@ -14,44 +16,93 @@ const ProductCarousel: React.FC<ProductCarouselProps> = ({
   products,
   title,
 }) => {
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const scrollAmount = 1500;
+
+  const handleScroll = (direction: 'prev' | 'next') => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft +=
+        direction === 'prev' ? -scrollAmount : scrollAmount;
+    }
+  };
+
   return (
     <Box className={styles.productImages}>
       <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-        {title} {/* Use the title prop */}
+        {title}
       </Typography>
-      <Box
-        className={styles.productCarousel}
-        sx={{
-          display: 'flex',
-          overflowX: 'auto',
-          '&::-webkit-scrollbar': {
-            height: '8px',
-          },
-          '&::-webkit-scrollbar-thumb': {
-            backgroundColor: 'rgba(0, 0, 0, 0.2)',
-            borderRadius: '4px',
-          },
-        }}
-      >
-        {products.map((product, index) => (
-          <Box key={index} sx={{ flexShrink: 0 }}>
-            <Button
-              href={`/products/${product.id}`}
-              className={styles.productImage}
-            >
-              <CardActionArea>
-                <Box className={styles.imageContainer}>
-                  <Image
-                    src={product.data.image}
-                    alt={`Product ${index + 1}`}
-                    layout="fill"
-                    objectFit="contain"
-                  />
-                </Box>
-              </CardActionArea>
-            </Button>
-          </Box>
-        ))}
+      <Box sx={{ position: 'relative' }}>
+        <Box
+          ref={carouselRef}
+          className={styles.productCarousel}
+          sx={{
+            display: 'flex',
+            overflowX: 'auto',
+            scrollBehavior: 'smooth',
+          }}
+        >
+          {products.map((product, index) => (
+            <Box key={index} sx={{ flexShrink: 0, marginRight: '16px' }}>
+              <Button
+                href={`/products/${product.id}`}
+                className={styles.productImage}
+              >
+                <CardActionArea>
+                  <Box className={styles.imageContainer}>
+                    <Image
+                      src={product.data.image}
+                      alt={`Product ${index + 1}`}
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </Box>
+                </CardActionArea>
+              </Button>
+            </Box>
+          ))}
+        </Box>
+        <Button
+          sx={{
+            position: 'absolute',
+            left: 0,
+            top: '50%',
+            transform: 'translateY(-50%)',
+            zIndex: 1,
+            opacity: 1.0,
+            padding: '24px 16px',
+            minWidth: 'auto',
+            height: '60%',
+            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            '&:hover': {
+              opacity: 1,
+              backgroundColor: 'white',
+            },
+          }}
+          onClick={() => handleScroll('prev')}
+        >
+          <ArrowBackIosIcon />
+        </Button>
+        <Button
+          sx={{
+            position: 'absolute',
+            right: 0,
+            top: '50%',
+            transform: 'translateY(-50%)',
+            zIndex: 1,
+            opacity: 1.0,
+            padding: '24px 16px',
+            minWidth: 'auto',
+            height: '60%',
+            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            '&:hover': {
+              opacity: 1,
+              backgroundColor: 'white',
+            },
+          }}
+          onClick={() => handleScroll('next')}
+        >
+          <ArrowForwardIosIcon />
+        </Button>
       </Box>
     </Box>
   );
