01f6c7fc78e108c357d67ea68abf381d70c2f2d2
tryles <trevorryles02@gmail.com>
2024-05-30 13:31:09 -0700

    Fix lint #233

--- Shopper/test/views/products/ProductCarousel.test.tsx
+++ Shopper/test/views/products/ProductCarousel.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { enqueueSnackbar, SnackbarProvider } from 'notistack';
 import { AppContext } from '@/contexts/AppContext';
-import ProductCarousel from "@/views/ProductCarousel";
+import ProductCarousel from '@/views/ProductCarousel';
 
 const server = setupServer();
 
@@ -29,21 +29,23 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-const products = [{
-  id: '123',
-  quantity: 1,
-  data: {
-    brand: 'nature valley',
-    name: 'Nature Valley Crunchy Granola Bars Oats N Honey',
-    rating: '4.5',
-    price: 3.68,
-    deliveryDate: '2021-10-10',
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/81A0NlBnRNL._SL1500_.jpg',
-    description:
-      'GRANOLA BAR: Nature Valley granola bars are made with whole grain oats and sweet honey flavor.',
-  }
-}];
+const products = [
+  {
+    id: '123',
+    quantity: 1,
+    data: {
+      brand: 'nature valley',
+      name: 'Nature Valley Crunchy Granola Bars Oats N Honey',
+      rating: '4.5',
+      price: 3.68,
+      deliveryDate: '2021-10-10',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/I/81A0NlBnRNL._SL1500_.jpg',
+      description:
+        'GRANOLA BAR: Nature Valley granola bars are made with whole grain oats and sweet honey flavor.',
+    },
+  },
+];
 
 describe('ProductCarousel', () => {
   it('Should load product carousel', () => {
