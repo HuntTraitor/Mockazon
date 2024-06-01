f0b24ae06478ee6e807b0f8d0a32f9a4602d01b3
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-06-01 03:02:59 -0700

    #220 Tests in the graphql of productCount

--- Shopper/test/api/product.test.ts
+++ Shopper/test/api/product.test.ts
@@ -16,6 +16,7 @@ let server: http.Server<
 let noErrorInProduct = true;
 let noErrorInProducts = true;
 let errorInSearchSuggestions = false;
+let noErrorInProductCount = true;
 
 const handlers = [
   rest.get(
@@ -39,6 +40,16 @@ const handlers = [
         ],
         { status: 200 }
       );
+    }
+  ),
+  rest.get(
+    `http://${process.env.MICROSERVICE_URL || 'localhost'}:3011/api/v0/product/count`,
+    async() => {
+      if (noErrorInProductCount) {
+        return HttpResponse.json(45, {status: 200})
+      } else {
+        return HttpResponse.json({message: 'Internal server error'}, {status: 500})
+      }
     }
   ),
   rest.get(
@@ -263,3 +274,21 @@ test('Gets search suggestions with error', async () => {
   expect(result.body.data).toBeNull();
   expect(result.body.errors[0].message).toBe('Internal Server Error');
 });
+
+test('Gets product count successfully', async() => {
+  const result = await supertest(server).post('/api/graphql').send({
+    query: `query getProductCount{getProductCount}`
+  });
+  expect(result.body.data).toBeDefined();
+  expect(result.body.data.getProductCount).toBeDefined();
+  expect(result.body.data.getProductCount).toBe(45)
+})
+
+test('Gets product count failure', async() => {
+  noErrorInProductCount = false;
+  const result = await supertest(server).post('/api/graphql').send({
+    query: `query getProductCount{getProductCount}`
+  });
+  expect(result.body.errors).toBeDefined();
+  expect(result.body.errors[0].message).toBe('Internal Server Error');
+})
