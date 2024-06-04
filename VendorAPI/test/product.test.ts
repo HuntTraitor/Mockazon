38e6e957dd780feb1c5155712abd92f9643aea3e
elkrishn <elkrishn@ucsc.edu>
2024-06-03 23:57:26 -0700

    #256

--- VendorAPI/test/product.test.ts
+++ VendorAPI/test/product.test.ts
@@ -6,3 +6,10 @@ describe('General', () => {
     await supertest(server).get('/v0/product/').expect(401);
   });
 });
+
+test('Authorized user can fetch product', async () => {
+  await supertest(server)
+    .get('/v0/product/')
+    .set('x-api-key', `bf582726-1927-4604-8d94-7f1540a7eb37`)
+    .expect(200)
+});
