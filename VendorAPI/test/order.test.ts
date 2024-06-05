89704ae7d7b44d3b269e8493be707c0e6f43e8db
elkrishn <elkrishn@ucsc.edu>
2024-06-04 20:37:01 -0700

    test/order.test.ts #256

--- VendorAPI/test/order.test.ts
+++ VendorAPI/test/order.test.ts
@@ -1,5 +1,13 @@
 import supertest from 'supertest';
 import { server } from './helper';
+
+// const orderUpdateData = {
+//   quantity: '4',
+//   shipped: true,
+// };
+
+// let orderID: string = '';
+
 test('Unauthorized user cannot fetch orders', async () => {
   await supertest(server).get('/v0/order/').expect(401);
 });
@@ -13,3 +21,15 @@ test('Authorized user can fetch orders', async () => {
       expect(res.body.length).toBeGreaterThan(0)
     })
 });
+
+// test('Vendor update order', async () => {
+//   console.log(orderID)
+//   await supertest(server)
+//     .put('/v0/order/ecddd831-d753-46ce-9c8b-ab37201b74a8')
+//     .set('x-api-key', 'bf582726-1927-4604-8d94-7f1540a7eb37')
+//     // .send(orderUpdateData)
+//     .expect(201)
+//     .then((res) => {
+//       console.log(res)
+//     })
+// });
