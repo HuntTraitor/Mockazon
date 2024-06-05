96641109d302a5c9cb861399d88efa570f854218
elkrishn <elkrishn@ucsc.edu>
2024-06-05 09:17:42 -0700

    #241 Test for Vendor Sign Up

---
+++ VendorApp/e2e/SignupForm.test.ts
@@ -0,0 +1,59 @@
+import { screen } from '@testing-library/dom';
+import puppeteer, { Browser, Page } from 'puppeteer';
+
+describe('Next.js App', () => {
+  let browser: Browser;
+  let page: Page;
+  /**
+   * Create a headless (not visible) instance of Chrome for each test
+   * and open a new page (tab).
+   */
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+    });
+    page = await browser.newPage();
+  });
+
+  /**
+   * Shotdown the API server then the Web server.
+   */
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  /**
+   * Close the headless instance of Chrome as we no longer need it.
+   */
+  afterEach(async () => {
+    await browser.close();
+  });
+
+  test('Successful Signup', async () => {
+    await page.goto('http://localhost:3003/login');
+    const name = await page.$('aria/name-input[role=input]');
+    const email = await page.$('aria/email-input[role=input]');
+    const password = await page.$('aria/password-input[role=input]');
+    const repeatPassword = await page.$(
+      'aria/repeatpassword-input[role=input]'
+    );
+
+    if (name && email && password && repeatPassword) {
+      await name.type('CSE187 Student');
+      await email.type('cse187student@vendor.com');
+      await password.type('password');
+      await repeatPassword.type('password');
+
+      await page.click('aria/submit-request');
+
+      await page.waitForFunction(() => {
+        const element = document.querySelector('body');
+        return (
+          element &&
+          element.textContent &&
+          element.textContent.includes('Successfully requested account!')
+        );
+      });
+    }
+  });
+});
