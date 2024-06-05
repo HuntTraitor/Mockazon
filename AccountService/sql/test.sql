33f2fe5fa7d55a1af0f2829f2265f47dbcf1ae75
elkrishn <elkrishn@ucsc.edu>
2024-06-04 22:06:59 -0700

    #256 Revert test.sqls

--- AccountService/sql/test.sql
+++ AccountService/sql/test.sql
@@ -5,7 +5,6 @@ DELETE FROM request;
 -- Admin Test Data -- 
 INSERT INTO shopper(id, data) VALUES ('81c689b1-b7a7-4100-8b2d-309908b444f5', jsonb_build_object( 'email', 'shopper@email.com', 'pwhash', crypt('pass', '87'), 'name', 'shopper account 1', 'username', 'shopperaccount', 'role', 'shopper', 'suspended', false));
 INSERT INTO vendor(id, data) VALUES ('81c689b1-b7a7-4100-8b2d-309908b444f6', jsonb_build_object( 'email', 'vendor@email.com', 'pwhash', crypt('pass', '87'), 'name', 'vendor account 1', 'username', 'vendoraccount', 'role', 'vendor', 'suspended', false));
-INSERT INTO vendor (id, data) VALUES('4f061f79-e0e8-48ff-a2ac-0a56a8ad5f0e', jsonb_build_object('email', 'htratar@ucsc.edu', 'pwhash', crypt('pass', '87'), 'name', 'Hunter Tratar', 'username', 'huntertratar', 'role', 'vendor', 'suspended', false));
 INSERT INTO request(id, data) VALUES ('81c689b1-b7a7-4100-8b2d-309908b444f7', jsonb_build_object( 'email', 'request1@email.com', 'pwhash', crypt('pass', '87'), 'name', 'request account 1', 'username', 'requestaccount1', 'role', 'vendor', 'suspended', false));
 INSERT INTO request(id, data) VALUES ('81c689b1-b7a7-4100-8b2d-309908b444f8', jsonb_build_object( 'email', 'request2@email.com', 'pwhash', crypt('pass', '87'), 'name', 'request account 2', 'username', 'requestaccount2', 'role', 'vendor', 'suspended', false));
 
--- KeyService/sql/test.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM api_key;
-INSERT INTO api_key(key, vendor_id, active, blacklisted) VALUES ('bf582726-1927-4604-8d94-7f1540a7eb37', '4f061f79-e0e8-48ff-a2ac-0a56a8ad5f0e', true, false);
