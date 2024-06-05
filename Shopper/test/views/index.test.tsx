872e5cbb2b4d40aa42efab3b549f56f8a3a792ae
tryles <trevorryles02@gmail.com>
2024-06-04 19:00:44 -0700

    Code coverage for pages/index.tsx #260

--- Shopper/test/views/index.test.tsx
+++ Shopper/test/views/index.test.tsx
@@ -1,6 +1,15 @@
 import { render } from '@testing-library/react';
 import { AppContext } from '@/contexts/AppContext';
 import Index, { getServerSideProps } from '../../src/pages/index';
+import { enqueueSnackbar } from 'notistack';
+import React from 'react';
+
+jest.mock('notistack', () => ({
+  ...jest.requireActual('notistack'),
+  enqueueSnackbar: jest.fn(),
+}));
+
+(enqueueSnackbar as jest.Mock).mockImplementation(jest.fn());
 
 // https://chat.openai.com/share/0c95f9af-f8b8-49a7-b104-efd929b218e7
 jest.mock('next/router', () => ({
@@ -45,36 +54,38 @@ const AppContextProps = {
   setAccountDrawerOpen: jest.fn(),
 };
 
-it('passes', () => {
-  expect(1).toBe(1);
+// it('passes', () => {
+//   expect(1).toBe(1);
+// });
+
+it('Renders', async () => {
+  const WrappedIndex = Index.getLayout(<Index />);
+
+  render(
+    <AppContext.Provider value={AppContextProps}>
+      {WrappedIndex}
+    </AppContext.Provider>
+  );
 });
 
-// it('Renders', async () => {
-//   render(
-//     <AppContext.Provider value={AppContextProps}>
-//       <Index />
-//     </AppContext.Provider>
-//   );
-// });
+jest.mock('next-i18next/serverSideTranslations', () => ({
+  serverSideTranslations: jest.fn().mockReturnValue({
+    en: {
+      common: {
+        title: 'Mock Title',
+      },
+    },
+  }),
+}));
 
-// jest.mock('next-i18next/serverSideTranslations', () => ({
-//   serverSideTranslations: jest.fn().mockReturnValue({
-//     en: {
-//       common: {
-//         title: 'Mock Title',
-//       },
-//     },
-//   }),
-// }));
+it('should fetch server side props with translations', async () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  await getServerSideProps({ locale: 'en' });
+});
 
-// it('should fetch server side props with translations', async () => {
-//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//   // @ts-expect-error
-//   await getServerSideProps({ locale: 'en' });
-// });
-
-// it('should fetch server side props with translations and null locale', async () => {
-//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//   // @ts-expect-error
-//   await getServerSideProps({ locale: null });
-// });
+it('should fetch server side props with translations and null locale', async () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  await getServerSideProps({ locale: null });
+});
