8763a836722ff97b392c3b1c969fc4d607b479c0
Luke <22577678+Iuke121@users.noreply.github.com>
2024-06-05 00:16:53 -0700

    100% coverage #262
    consider re-adding stripe error handling

--- OrderService/test/stripeCheckout.test.ts
+++ OrderService/test/stripeCheckout.test.ts
@@ -2,79 +2,71 @@ import supertest from 'supertest';
 import { server } from './helper';
 import { randomUUID } from 'crypto';
 
-// test('Successfully creates checkout session 201', async () => {
-//   const result = await supertest(server)
-//     .post(`/api/v0/stripeCheckout`)
-//     .send({
-//       lineItems: [
-//         {
-//           price_data: {
-//             currency: 'usd',
-//             unit_amount: 1000,
-//             product_data: {
-//               name: 'T-Shirt',
-//               images: ['https://example.com/t-shirt.png'],
-//             },
-//           },
-//           quantity: 3,
-//         },
-//       ],
-//       shopperId: { shopperId: randomUUID() },
-//       origin: 'http://localhost:3000',
-//       locale: 'en',
-//       metadata: {
-//         items: [{ productId: randomUUID(), vendorId: randomUUID() }],
-//       },
-//     });
-//   expect(result.status).toBe(201);
-//   expect(result.body.id).toBeDefined();
-//   expect(result.body.url).toBeDefined();
-// });
+test('Successfully creates checkout session 201', async () => {
+  const lineItem = {
+    price_data: {
+      currency: 'usd',
+      unit_amount: 1000,
+      product_data: {
+        name: 'T-Shirt',
+        images: ['https://example.com/t-shirt.png'],
+        metadata: {
+          productId: randomUUID(),
+          vendorId: randomUUID(),
+        },
+      },
+    },
+    quantity: 3,
+  };
 
-// test('Successfully creates checkout session in spanish 201', async () => {
-//   const result = await supertest(server)
-//     .post(`/api/v0/stripeCheckout`)
-//     .send({
-//       lineItems: [
-//         {
-//           price_data: {
-//             currency: 'usd',
-//             unit_amount: 1000,
-//             product_data: {
-//               name: 'T-Shirt',
-//               images: ['https://example.com/t-shirt.png'],
-//             },
-//           },
-//           quantity: 3,
-//         },
-//       ],
-//       shopperId: { shopperId: randomUUID() },
-//       origin: 'http://localhost:3000',
-//       locale: 'es',
-//       metadata: {
-//         items: [{ productId: randomUUID(), vendorId: randomUUID() }],
-//       },
-//     });
-//   expect(result.status).toBe(201);
-//   expect(result.body.id).toBeDefined();
-//   expect(result.body.url).toBeDefined();
-// });
+  const sessionInput = {
+    lineItems: [lineItem],
+    shopperId: { shopperId: randomUUID() },
+    origin: 'http://localhost:3000',
+    locale: 'en',
+  };
 
-// test('Failing to create checkout session due to invalid lineItems 400', async () => {
-//   const result = await supertest(server)
-//     .post(`/api/v0/stripeCheckout`)
-//     .send({
-//       lineItems: [],
-//       shopperId: { shopperId: randomUUID() },
-//       origin: 'http://localhost:3000',
-//       locale: 'en',
-//       metadata: {
-//         items: [{ productId: randomUUID(), vendorId: randomUUID() }],
-//       },
-//     });
-//   expect(result.status).toBe(400);
-//   expect(result.body.message).toBeDefined();
-// });
+  const result = await supertest(server)
+    .post(`/api/v0/stripeCheckout`)
+    .send(sessionInput);
+
+  expect(result.status).toBe(201);
+  expect(result.body.id).toBeDefined();
+  expect(result.body.url).toBeDefined();
+});
+
+test('Successfully creates checkout session 201', async () => {
+  const lineItem = {
+    price_data: {
+      currency: 'usd',
+      unit_amount: 1000,
+      product_data: {
+        name: 'T-Shirt',
+        images: ['https://example.com/t-shirt.png'],
+        metadata: {
+          productId: randomUUID(),
+          vendorId: randomUUID(),
+        },
+      },
+    },
+    quantity: 3,
+  };
+
+  const sessionInput = {
+    lineItems: [lineItem],
+    shopperId: { shopperId: randomUUID() },
+    origin: 'http://localhost:3000',
+    locale: 'es',
+  };
+
+  const result = await supertest(server)
+    .post(`/api/v0/stripeCheckout`)
+    .send(sessionInput);
+
+  expect(result.status).toBe(201);
+  expect(result.body.id).toBeDefined();
+  expect(result.body.url).toBeDefined();
+});
 
 test('Failing to create checkout session due to missing lineItems 400', async () => {
   const result = await supertest(server)
