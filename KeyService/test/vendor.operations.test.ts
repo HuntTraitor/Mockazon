87a2ebdf5f7e5d3dc9ce2b256bcb7af61bf071e5
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-06-05 17:43:44 -0700

    #263 npm run lint

--- KeyService/test/vendor.operations.test.ts
+++ KeyService/test/vendor.operations.test.ts
@@ -34,54 +34,52 @@ describe('Vendor Operations', () => {
       });
   });
 
-  test('Validate the api key with a valid vendorId', async() => {
+  test('Validate the api key with a valid vendorId', async () => {
     await supertest(server)
       .get(`/api/v0/key/validate?apiKey=${keys[0]}`)
       .expect(200)
       .then(res => {
         expect(res.body.vendor_id).toBe(vendorId);
-      })
-  })
+      });
+  });
 
-  test('Validate api key with an unknown key', async() => {
+  test('Validate api key with an unknown key', async () => {
     await supertest(server)
       .get(`/api/v0/key/validate?apiKey=${randomUUID()}`)
-      .expect(401)
-  })
+      .expect(401);
+  });
 
-  test('Validate an api key that is not inputted', async() => {
-    await supertest(server)
-      .get(`/api/v0/key/validate`)
-      .expect(400)
-  })
+  test('Validate an api key that is not inputted', async () => {
+    await supertest(server).get(`/api/v0/key/validate`).expect(400);
+  });
 
-  test('Deactive the current api key (success)', async() => {
+  test('Deactive the current api key (success)', async () => {
     await supertest(server)
       .put(`/api/v0/key/${keys[0]}/active`)
       .expect(200)
       .then(res => {
-        expect(res.body).toBeDefined()
-        expect(res.body.active).toBe(false)
-        expect(res.body.key).toBe(keys[0])
-        expect(res.body.vendor_id).toBe(vendorId)
-      })
-  })
+        expect(res.body).toBeDefined();
+        expect(res.body.active).toBe(false);
+        expect(res.body.key).toBe(keys[0]);
+        expect(res.body.vendor_id).toBe(vendorId);
+      });
+  });
 
-  test('Flip the current api key (failure)', async() => {
+  test('Flip the current api key (failure)', async () => {
     await supertest(server)
       .put(`/api/v0/key/${randomUUID()}/active`)
-      .expect(409)
-  })
+      .expect(409);
+  });
 
-  test('Reactivate the current api key (success)', async() => {
+  test('Reactivate the current api key (success)', async () => {
     await supertest(server)
       .put(`/api/v0/key/${keys[0]}/active`)
       .expect(200)
       .then(res => {
-        expect(res.body).toBeDefined()
-        expect(res.body.active).toBe(true)
-        expect(res.body.key).toBe(keys[0])
-        expect(res.body.vendor_id).toBe(vendorId)
-      })
-  })
+        expect(res.body).toBeDefined();
+        expect(res.body.active).toBe(true);
+        expect(res.body.key).toBe(keys[0]);
+        expect(res.body.vendor_id).toBe(vendorId);
+      });
+  });
 });
