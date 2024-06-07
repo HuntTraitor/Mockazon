2a7c877ef84b73c2a56d98cb800bbd028fe1ce7c
elkrishn <elkrishn@ucsc.edu>
2024-06-07 13:57:38 -0700

    Admin e2e view all users

---
+++ Admin/e2e/Users.test.ts
@@ -0,0 +1,60 @@
+import puppeteer, { Browser, Page } from 'puppeteer';
+
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('Admin List of Users Test', () => {
+  let browser: Browser;
+  let page: Page;
+  /**
+   * Create a headless (not visible) instance of Chrome for each test
+   * and open a new page (tab).
+   */
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+    });
+    page = await browser.newPage();
+  });
+
+  /**
+   * Shotdown the API server then the Web server.
+   */
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  /**
+   * Close the headless instance of Chrome as we no longer need it.
+   */
+  afterEach(async () => {
+    await browser.close();
+  });
+
+  test('Users List in Admin Home Page', async () => {
+    // Sign in
+    await page.goto(
+      `http://${process.env.MICROSERVICE_URL || 'localhost'}:3001/admin`
+    );
+    await page.type('aria/Email Address[role="textbox"]', 'htratar@ucsc.edu');
+    await page.type('aria/Password[role="textbox"]', 'pass');
+    await page.click('aria/login-button[role="button"]');
+    await page.waitForFunction(() => {
+      const element = document.querySelector('body');
+      return (
+        element && element.textContent && element.textContent.includes('Role')
+      );
+    });
+    await sleep(1000);
+    await page.waitForFunction(() => {
+      const element = document.querySelector('body');
+      // Can view one of the users
+      return (
+        element && element.textContent && element.textContent.includes('shopper')
+        && element.textContent.includes('Evan Metcalf')
+      );
+    });
+  });
+});
